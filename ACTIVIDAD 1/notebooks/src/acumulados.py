cef392eabdf8c6c544dcc763458ea00605b2188f
Melina <melinaarriaran@gmail.com>
2025-09-26 19:28:35 -0300

    detalles finales de orden

--- ACTIVIDAD 1/notebooks/src/acumulados.py
+++ ACTIVIDAD 1/notebooks/src/acumulados.py
@@ -2,23 +2,15 @@
 Contiene funciones para inicializar y actualizar los acumulados por equipo.
 """
 
-
 def resetear_valores(): #-> diccionario
     return {"innovacion": 0, "presentacion": 0, "errores": 0, "mejores": 0, "total": 0}
 
-#Utilicé lambda y map únicamente para ya asegurarnos de usarlos en algun lado,
-#no es lo más eficiente, preguntar si lo hacemos así o con dict comprehension
 def inicializar_acumulados(evaluaciones):   
     equipos = evaluaciones[0].keys()
     acumulados = dict(map(lambda equipo: (equipo, resetear_valores()), equipos))    #map recorre la lista de equipos(nombres), lambda recibe el argumento del nombre del equipo,
     return acumulados                                                               #y con el diccionario reseteado por la función, crea una tupla ("Nombre Equipo",{"innovacion":0,"errores":0,...}), luego esa lista de tuplas es dict()
-                                                                                                                                                      
-#En el programa principal se puede probar así:
-# !! (import pprint) para imprimir lindo el diccionario)
-#acum = inicializar_acumulados(evaluaciones)
-#pprint.porint(acum)
 
-
+                                                                                                                                              
 def actualizar_acumulados(acum: dict, ronda: dict, mejor: str) -> dict: 
     for equipo in ronda:
         acum[equipo]["innovacion"] += ronda[equipo]["innovacion"]
